31fab3079acb80a2b2f15fd0928b1235deabee3e
Rachel Navarro <rtnavarr@uci.edu>
2021-01-29 21:50:09 -0800

    add link to google form

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Linking, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to consistent.ly!</Text>
+      <Text style={{color: 'blue'}}
+          onPress={() => Linking.openURL('https://forms.gle/QwniWfidR5jZ62vFA')}>
+      Fill out our Google Form here.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
