fd807f7ab86703bc6f8444469666aee267aea45f
Fatima Ali <alifatima168@gmail.com>
2021-02-06 22:10:00 -0800

    pls

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-<<<<<<< HEAD
 // import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { name as appName } from './app.json';
-=======
-import React from 'react';
-import { StyleSheet, Linking, Text, View } from 'react-native';
->>>>>>> 00dac3bb7f54faca6f2f43c1513053a30b979874
 
 const App = () =>  {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-      <Text>Open up App.js to start working on your app!</Text>
-      
-=======
       <Text style={{color: 'white'}}>Welcome to consistent.ly!</Text>
       <Text style={{color: 'blue'}}
           onPress={() => Linking.openURL('https://forms.gle/QwniWfidR5jZ62vFA')}>
       Fill out our Google Form here.
       </Text>
->>>>>>> 00dac3bb7f54faca6f2f43c1513053a30b979874
       <StatusBar style="auto" />
     </View>
   
