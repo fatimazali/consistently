5548b22eafb7cdbaf33c0627eb2150f7f47197f6
Rachel Navarro <rtnavarr@uci.edu>
2021-02-18 13:32:39 -0800

    add searchbar to search page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,29 @@
 import { Text } from 'react-native';
 import React, { Component } from 'react';
 import styles from './Styles.js';
-import { IconButton, Dialog, Colors , Modal, Portal, Button, Provider} from 'react-native-paper';
+import { IconButton, Dialog, Colors , Searchbar, Modal, Portal, Button, Provider} from 'react-native-paper';
 
 class Search extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        visible: false
+        visible: false,
+        searchQuery: '',
       };
     };
     
     render() {
       const hideDialog = () => this.setState({ visible: false });
       const showDialog = () => this.setState({ visible: true });
+      const onChangeSearch = query => this.setState({searchQuery : query});
 
       return (
         <Provider>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={this.state.searchQuery}
+          />
           <Portal>
             <Dialog visible={this.state.visible} onDismiss={hideDialog} contentContainerStyle={{backgroundColor: 'white', padding: 20}}>
               <Dialog.Content>
