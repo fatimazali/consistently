fcd9b88a7ce9a2c230e6b5bb0fcc1a55723d93bb
Michelle Liang <michelleliang001@gmail.com>
2021-02-18 18:00:40 -0800

    add cards to recommendation page

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import React, { Component } from 'react';
 import styles from './Styles.js';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
@@ -6,7 +6,7 @@ import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 class Recommendation extends Component {
   render() {
     return (
-        <View>
+        <ScrollView>
             <Text style={styles.pageHeader}>             Top 3 Picks</Text> 
             <Card>
                 <Card.Content>
@@ -19,7 +19,29 @@ class Recommendation extends Component {
                     <Button>Ok</Button>
                 </Card.Actions>
             </Card>
-        </View>
+            <Card>
+                <Card.Content>
+                    <Title>Dancing</Title>
+                    <Paragraph>Card content</Paragraph>
+                </Card.Content>
+                <Card.Cover source={require('../images/dancing.png')} />
+                <Card.Actions>
+                    <Button>Cancel</Button>
+                    <Button>Ok</Button>
+                </Card.Actions>
+            </Card>
+            <Card>
+                <Card.Content>
+                    <Title>Core Training</Title>
+                    <Paragraph>Card content</Paragraph>
+                </Card.Content>
+                <Card.Cover source={require('../images/core.png')} />
+                <Card.Actions>
+                    <Button>Cancel</Button>
+                    <Button>Ok</Button>
+                </Card.Actions>
+            </Card>
+        </ScrollView>
     )
     
   }
