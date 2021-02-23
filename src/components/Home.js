63e74d2bae9760edcafc575207cd9874cdc1e51c
Rachel Navarro <rtnavarr@uci.edu>
2021-02-22 19:00:18 -0800

    add questions to daily check-in + checkbox functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { Text, View, Linking, ScrollView} from 'react-native';
+import { Text, View, Linking, Picker, ScrollView} from 'react-native';
 import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import styles from './Styles';
@@ -12,8 +12,14 @@ class Home extends Component {
     this.state = {
       visible: false,
       hasSignedUp: true,
-      text: '',
-      value: 'first',
+      intensity: 'first',
+      focus: 'first',
+      duration: '15',
+      affirmation: 'first',
+      hasWeights: false,
+      hasMat: false,
+      hasBike: false,
+      hasStepmill: false,
     };
   };
   
@@ -33,8 +39,15 @@ class Home extends Component {
       const showDialog = () => this.setState({ visible: true });
       const hideDialog = () => this.setState({ visible: false });
 
-      const setButtonChoice = value => this.setState({ value: value });
-      const setTextEntered = text => this.setState({ text: text });
+      const setIntensity = value => this.setState({ intensity: value });
+      const setFocus = value => this.setState({ focus: value });
+      const setDuration = value => this.setState({ duration: value });
+      const setAffirmation = value => this.setState({ affirmation: value });
+
+      const setHasWeights = () => this.setState({ hasWeights: !this.state.hasWeights });
+      const setHasMat = () => this.setState({ hasMat: !this.state.hasMat });
+      const setHasBike = () => this.setState({ hasBike: !this.state.hasBike });
+      const setHasStepmill = () => this.setState({ hasStepmill: !this.state.hasStepmill });
   
       const containerStyle = {backgroundColor: 'white', padding: 50};
       
@@ -59,29 +72,60 @@ class Home extends Component {
                 <Dialog.Content>
     
     
-                  <Subheading>Today I prefer to...</Subheading>
-                    <RadioButton.Group onValueChange={setButtonChoice} value={this.state.value}>
-                      <RadioButton.Item label="slow down and recover!" value="gentle" />
-                      <RadioButton.Item label="take it easy!" value="easy" />
-                      <RadioButton.Item label="work up a good sweat!" value="moderate" />
-                      <RadioButton.Item label="challenge myself!" value="hard" />
-                      <RadioButton.Item label="go far beyond my comfort zone!" value="intense" />
+                  <Subheading>Today, I prefer to...</Subheading>
+                    <RadioButton.Group onValueChange={setIntensity} value={this.state.intensity}>
+                      <RadioButton.Item label="take it easy! (light)" value="light" />
+                      <RadioButton.Item label="work up a good sweat! (moderate)" value="moderate" />
+                      <RadioButton.Item label="challenge myself! (vigorous)" value="vigorous" />
+                      <RadioButton.Item label="gO hArD or gO hOmE! (extreme)" value="extreme" />
                     </RadioButton.Group>
                   <Text> </Text>
-    
+
+                  <Subheading>Today, I want to focus on my...</Subheading>
+                    <RadioButton.Group onValueChange={setFocus} value={this.state.focus}>
+                      <RadioButton.Item label="lower body" value="lower" />
+                      <RadioButton.Item label="upper body" value="upper" />
+                      <RadioButton.Item label="whole body" value="whole" />
+                      <RadioButton.Item label="abodominals" value="abdominal" />
+                    </RadioButton.Group>
+                  <Text> </Text>
+                  
+
                   <Subheading>I have __ min to workout today.</Subheading>
-                    <TextInput
-                      label="Enter min..."
-                      value={this.state.text}
-                      onChangeText={setTextEntered}
-                    />
+                  <Picker
+                    selectedValue={this.state.duration}
+                    style={{ height: 50, width: 150 }}
+                    onValueChange={setDuration}
+                  >
+                    <Picker.Item label="15" value="15" />
+                    <Picker.Item label="30" value="30" />
+                    <Picker.Item label="45" value="45" />
+                    <Picker.Item label="60" value="60" />
+                    <Picker.Item label="75" value="75" />
+                    <Picker.Item label="90" value="90" />
+                  </Picker>
                   <Text> </Text>
     
                   <Subheading>My available equipment includes...</Subheading>
-                      <Checkbox.Item label="weights" status="unchecked" />
-                      <Checkbox.Item label="yoga mat" status="unchecked" />
-                      <Checkbox.Item label="bike/stationary bike" status="unchecked" />
-                      <Checkbox.Item label="bench press" status="unchecked" />
+                      <Checkbox.Item onPress={setHasWeights} label="weights" status={this.state.hasWeights ? 'checked' : 'unchecked'} />
+                      <Checkbox.Item onPress={setHasMat} label="yoga mat" status={this.state.hasMat ? 'checked' : 'unchecked'} />
+                      <Checkbox.Item onPress={setHasBike} label="bike/stationary bike" status={this.state.hasBike ? 'checked' : 'unchecked'} />
+                      <Checkbox.Item onPress={setHasStepmill} label="stepmill" status={this.state.hasStepmill ? 'checked' : 'unchecked'} />
+                  <Text> </Text>
+
+                  <Subheading>Today's affirmation:</Subheading>
+                    <Picker
+                    selectedValue={this.state.affirmation}
+                    style={{ height: 50, width: 450 }}
+                    onValueChange={setAffirmation}
+                  >
+                    <Picker.Item label="I am a workout warrior!" value="aff1" />
+                    <Picker.Item label="I am becoming stronger both mentally and physically!" value="aff2" />
+                    <Picker.Item label="Soreness is weakness leaving my body!" value="aff3" />
+                    <Picker.Item label="Hustle for that muscle!" value="aff4" />
+                    <Picker.Item label="Let’s release those endorphins!" value="aff5"  />
+                  </Picker>
+                  <Text> </Text>
     
                 </Dialog.Content>
                 <Dialog.Actions>
